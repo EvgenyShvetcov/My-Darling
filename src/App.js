5f1ceb70d33cf9f3a81f756f88cae05a31b4289c
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2020-10-29 16:06:02 +0300

    switch remote repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment } from 'react';
+import { Navbar } from './components/Navbar'
+import { Main } from './components/Main'
+import { Contacts } from './components/Contacts';
+import { Menu } from './components/Menu'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Media from 'react-media';
+import video from './components/PorkVid.mp4'
+
+
+
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+        <div className="App">
+          <BrowserRouter>
+            <Navbar  />
+            <Media queries={{
+        small: "(max-width: 599px)",
+        large: "(min-width: 600px)"
+      }}>
+            {matches => (
+              <Fragment>
+                {matches.small && <><div>
+                  <div style={{
+                    height: '100%',
+                    width: '100%',
+                    position: 'fixed',
+                    backgroundColor: 'black',
+                    
+                    zIndex: '-1',
+                    left: '0',
+                    right: '0',
+                    bottom: '0',
+                    top: '0'
+                  }}></div>
+                  <div style={{
+                    position: 'relative',
+                  }}>
+                    <div style={{
+                      position: 'absolute', width: '100%', height: '100%', zIndex: '-2' }}>
+
+                    </div>
+                    </div></div></>}
+                    {matches.large && <div>
+                  <div style={{
+                    height: '100%',
+                    width: '100%',
+                    position: 'fixed',
+                    backgroundColor: 'black',
+                    opacity: '0.9',
+                    zIndex: '-1',
+                    left: '0',
+                    right: '0',
+                    bottom: '0',
+                    top: '0'
+                  }}></div>
+                  <div style={{
+                    position: 'relative',width: '100%', height: '100%'
+                  }}>
+                    <video autoPlay loop muted style={{
+                      position: 'fixed',  width: '125%', zIndex: '-2'
+                    }}alt=''>
+                      <source src={video} type="video/mp4"></source>
+                    </video></div></div>}
+
+            <Switch>
+              <Route exact path="/">
+                <Main  />
+              </Route>
+              <Route path="/menu">
+                <Menu />
+              </Route>
+              <Route path="/contacts">
+                <Contacts />
+              </Route>
+            </Switch>
+
+            </Fragment>)}
+            </Media>
+          </BrowserRouter>
+        </div>
+    </>
   );
 }
 
